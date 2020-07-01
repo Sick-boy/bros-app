93e991c973b4af86b45fdaaf8f8a9a25acc3d1f1
anton-varenyuk <anton.varenyuk@avenga.com>
2020-07-01 19:29:28 +0300

    branch1: Further development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './routes/homepage/homepage.component';
+import { SocialComponent } from './routes/social/social.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomepageComponent }
+  { path: 'social', component: SocialComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: '**', component: HomepageComponent }
 ];
 
 @NgModule({
